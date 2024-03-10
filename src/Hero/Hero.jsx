bc70b01f22e98d0789ee307e673ea8f0c619d3bc
aminetf1 <162189350+aminetf1@users.noreply.github.com>
2024-03-10 18:21:06 +0000

    Hero.jsx

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="hero-container">
         <div className="max-w-[1200px] m-auto flex flex-col-reverse md:flex-row">
             <div className="content-right px-4 py-14 flex-1 flex flex-col gap-10">
-                <h1 className='text-[35px] lg:text-[60px] font-[700] capitalize'>simple <span className='text-primary'>storage</span> solutions just for you.</h1>
+                <h1 className='text-[35px] lg:text-[60px] font-[700] capitalize'><span className='text-primary'>simple </span>storage solutions just for you.</h1>
                 <p className='capitalize text-[18px] text-slate-600 font-[400]'>summer storage made for UIT students - by UIT students</p>
                 <a href='https://d0jstai1zyk.typeform.com/to/J4A5Mjbx' className='bg-primary w-[200px] text-center px-6 py-4 rounded-md text-slate-50 uppercase font-[500] tracking-normal'>register now</a>
             </div>
